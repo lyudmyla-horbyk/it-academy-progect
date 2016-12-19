51ce6139909d72cbb4e0805c8b8566a8a949011e
Lyudmyla <lyudmyla.horbyk@gmail.com>
2016-12-19 23:58:55 +0200

    office page  with sliders and changes

--- scripts/slider.js
+++ scripts/slider.js
@@ -95,7 +95,27 @@ $(document).ready(function () {
   $('.slider5').slick({
     infinite: true,
     slidesToShow: 4,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
 
   });
   $('.slider5-header .left-arrow').on('click', function (e) {
